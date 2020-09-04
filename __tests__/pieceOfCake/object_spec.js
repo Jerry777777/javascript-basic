3e134237d4cd9f2c7362205ea68f41bcab0489ae
Jerry777777 <491818175@qq.com>
2020-09-04 17:07:21 +0800

    [xiawenhao] feat: should overriding property of prototype for an instance

--- __tests__/pieceOfCake/object_spec.js
+++ __tests__/pieceOfCake/object_spec.js
@@ -240,7 +240,7 @@ describe('for object', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expectedKillerRabbitTeeth = undefined;
+    const expectedKillerRabbitTeeth = 'small';
     // --end->
 
     expect(killerRabbit.teeth).toEqual(expectedKillerRabbitTeeth);
@@ -249,8 +249,8 @@ describe('for object', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expectedOverrideKillerRabbitTeeth = undefined;
-    const expectedRabbitPrototypeTeeth = undefined;
+    const expectedOverrideKillerRabbitTeeth = 'sharp';
+    const expectedRabbitPrototypeTeeth = 'small';
     // --end->
 
     expect(killerRabbit.teeth).toEqual(expectedOverrideKillerRabbitTeeth);
